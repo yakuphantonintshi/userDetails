c8cf83f06c3c77b28718a4bf25cf3b567364fe89
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-20 08:38:40 +0200

    try

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<form action="" class="container">
+  <input type="text" placeholder="First name" required v-model="firstName">
+  <input type="text" placeholder="Last name" required v-model="lastName">
+  <input type="text" placeholder="Gender" required v-model="gender" :class="{
+    male: lowerCaseGender == 'male', female: lowerCaseGender == 'female'
+  }">
+  <textarea output readonly ></textarea>
+  <button type="button" @click="userInfo">Display</button>
+
+
+</form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+    return {
+      firstName:'',
+      lastName:'',
+      gender:''
+    }
+  },
+  computed: {
+    lowerCaseGender(){
+      return this.gender.toLowerCase()
+    }
+  },
+  methods: {
+    userInfo(){
+      document.querySelector('[output]').textContent = `
+      First name: ${this.firstName}
+      Last name: ${this.lastName}
+      Gender: ${this.gender}
+      `
+    }
   }
 }
+
 </script>
 
 <style>
@@ -22,5 +51,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container{
+  display: grid;
+  gap: .5rem;
+  width: 30rem;
+  margin-inline: auto;
+  & input{
+    text-align: center;
+  }
+}
+.male{
+  background-color:blue;
+}
+.female{
+  background-color:rgb(249, 21, 150);
 }
 </style>
